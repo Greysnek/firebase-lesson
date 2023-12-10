8e8880dd1596a10b35b9c845da4a913268257dd1
Greysnek <i.semenov@bit.games>
2023-12-10 17:31:14 +0300

    Example 2 votes

---
+++ Assets/Scripts/Example2/Windows/VoteWindow/VoteSection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+namespace igrohub.Example2.Windows
+{
+  public class VoteSection : MonoBehaviour
+  {
+    [SerializeField] private Button _voteButton;
+    [SerializeField] private TMP_Text _header;
+    [SerializeField] private TMP_Text _output;
+
+    private string _name;
+    private Action<string> _onVote; 
+      
+    public void Init(string sectionName, Action<string> onVote)
+    {
+      _onVote = onVote;
+      _voteButton.onClick.AddListener(OnButtonClick);
+      _name = sectionName;
+      _header.text = sectionName;
+      
+      UpdateData(false, 0);
+    }
+
+    public void UpdateData(bool voted, long voteCount)
+    {
+      _output.text = voteCount.ToString();
+      _voteButton.gameObject.SetActive(!voted);
+      _output.gameObject.SetActive(voted);
+    }
+
+    private void OnButtonClick()
+    {
+      _onVote?.Invoke(_name);
+    }
+  }
+}
---
+++ Assets/Scripts/Example2/Windows/VoteWindow/VoteWindow.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Extensions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+namespace igrohub.Example2.Windows
+{
+  public class VoteWindow : WindowBehaviour
+  {
+    [SerializeField] private string[] _sectionsNames;
+    [Space]
+    [SerializeField] private TMP_Text _currentUserName;
+    [SerializeField] private Button _exit;
+    [SerializeField] private Transform _sectionsHolder;
+    [SerializeField] private VoteSection _sectionPrefab;
+
+    private FirebaseUser _user;
+    private DatabaseReference _databaseReference;
+    private readonly Dictionary<string, VoteSection> _sections = new();
+    private bool _voted;
+
+    private void Awake()
+    {
+      _exit.onClick.AddListener(Exit);
+      _databaseReference = FirebaseDatabase.DefaultInstance.RootReference.Root.Child("Example2").Child("Votes");
+      
+      foreach (var sectionName in _sectionsNames)
+      {
+        _sections[sectionName] = Instantiate(_sectionPrefab, _sectionsHolder);
+        _sections[sectionName].Init(sectionName, Vote);
+        _databaseReference.Child(sectionName).ChildChanged += UpdateData;
+      }
+    }
+    
+    protected override void OnShow()
+    {
+      _user = FirebaseAuth.DefaultInstance.CurrentUser;
+      _currentUserName.text = string.IsNullOrEmpty(_user.DisplayName) ? _user.Email : _user.DisplayName;
+      UpdateAllData();
+    }
+
+    private void UpdateAllData()
+    {
+      _databaseReference.GetValueAsync().ContinueWithOnMainThread(
+      task => {
+        var data = task.Result;
+
+        _voted = data.Children.Any(c => c.HasChild(_user.UserId));
+        
+        foreach (var section in _sections.Keys)
+        {
+          var sectionSnapShot = data.Child(section);
+          UpdateData(sectionSnapShot);
+        }
+      });
+    }
+
+    private void UpdateData(object sender, ChildChangedEventArgs args)
+    {
+      UpdateData(args.Snapshot);
+    }
+
+    private void UpdateData(DataSnapshot snapShot)
+    {
+      _sections[snapShot.Key].UpdateData(_voted, snapShot.ChildrenCount);
+    }
+
+    private static void Exit()
+    {
+      FirebaseAuth.DefaultInstance.SignOut();
+      WindowsController.Show<EntryWindow>();
+    }
+
+    private void Vote(string section)
+    {
+      _voted = true;
+      _databaseReference.Child(section).Child(_user.UserId).SetValueAsync("").ContinueWithOnMainThread(_ => UpdateAllData());
+    }
+  }
+}
